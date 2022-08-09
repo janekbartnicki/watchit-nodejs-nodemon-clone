45f45788ac537b30b3be23e59e85ae49d6ae96c9
Jan Bartnicki <53710293+janekbartnicki@users.noreply.github.com>
2022-08-10 00:08:49 +0200

    1

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
-console.log('im alive')
+const chokidar = require('chokidar');
+
+chokidar.watch('.', {ignoreInitial: true})
+    .on('add', (path, stats) => {
+        const {atime} = stats;
+        console.log(`ADDED ${path} [${atime.getHours()}:${atime.getMinutes()}.${atime.getSeconds()}]`)
+    })
+    .on('change', (path, stats) => {
+        console.log(`CHANGED ${path} [Current size: ${stats.size} chars]`)
+    })
+    .on('unlink', () => console.log('FILE REMOVED'))
