00180d228644ca506d7dfd79a5bd17173d6a016a
janekbartnicki <jachu01@poczta.onet.pl>
2022-08-13 14:32:17 +0200

    1

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 #!/usr/bin/env node
 
 const chokidar = require('chokidar');
+const program = require('caporal');
 
-chokidar.watch('.', {ignoreInitial: true})
-    .on('add', (path, stats) => {
-        const {atime} = stats;
-        console.log(`ADDED ${path} [${atime.getHours()}:${atime.getMinutes()}.${atime.getSeconds()}]`)
-    })
-    .on('change', (path, stats) => {
-        console.log(`CHANGED ${path} [Current size: ${stats.size} chars]`)
-    })
-    .on('unlink', () => console.log('FILE REMOVED'))
+// console.log('Watching for changes...');
+
+program
+    .version('0.0.1')
+    .argument('[filename]', 'Name of the file to watch')
+    .action((args) => {
+        console.log(args)
+    });
+
+program.parse(process.argv);
+
+// chokidar.watch('.', {ignoreInitial: true})
+//     .on('add', (path, stats) => {
+//         const {atime} = stats;
+//         console.log(`ADDED ${path} [${atime.getHours()}:${atime.getMinutes()}.${atime.getSeconds()}]`)
+//     })
+//     .on('change', (path, stats) => {
+//         console.log(`CHANGED ${path} [Current size: ${stats.size} chars]`)
+//     })
+//     .on('unlink', () => console.log('FILE REMOVED'))
