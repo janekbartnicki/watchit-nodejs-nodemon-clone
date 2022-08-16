717b9df3783f29cb789b8bb76e8d6d2b24c09991
janekbartnicki <jachu01@poczta.onet.pl>
2022-08-16 14:20:14 +0200

    1

--- index.js
+++ index.js
@@ -4,30 +4,35 @@ const chokidar = require('chokidar');
 const program = require('caporal');
 const fs = require('fs');
 const {spawn} = require('child_process');
-const { inherits } = require('util');
-
-console.log('\x1b[36m%s\x1b[0m', 'Watching for changes...');
+const {inherits} = require('util');
 
 program
     .version('0.0.1')
     .argument('[filename]', 'Name of the file to watch')
     .action(async ({filename}) => {
+        const name = filename || 'index.js';
         try{
-            await fs.promises.access(filename)
+            await fs.promises.access(name);
         } catch {
-            throw new Error(`Could not find the file ${filename}`)
+            throw new Error(`Could not find the file ${name}`)
         };
 
-        spawn('node', [filename], {stdio: 'inherit'});
-        chokidar.watch(filename || process.cwd(), {ignoreInitial: true})
+        let proc;
+        const start = () => {
+            if(proc) proc.kill();
+            console.log('\x1b[36m%s\x1b[0m', 'Watching for changes...');
+            proc = spawn('node', [name], {stdio: 'inherit'});
+        };
+        
+        chokidar.watch('.', {ignoreInitial: true})
             .on('add', (path, {atime}) => {
                 console.log('\x1b[32m', `ADDED ${path} [${atime.getHours()}:${atime.getMinutes()}.${atime.getSeconds()}]`);
-                spawn('node', [filename], {stdio: 'inherit'});
+                start();
 
             })
             .on('change', (path, stats) => {
                 console.log('\x1b[33m', `CHANGED ${path} [Current size: ${stats.size} chars]`)
-                spawn('node', [filename], {stdio: 'inherit'});
+                start();
             })
             .on('unlink', (path) => console.log('\x1b[31m', `FILE REMOVED [${path}]`));
     });
